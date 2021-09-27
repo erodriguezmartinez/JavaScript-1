a394a0c7a52bb20e112890ea80fb2890ef51e870
erodriguezmartinez <erodriguezmartinez.guadalupe@alumnado.fundacionloyola.net>
2021-09-27 21:20:37 +0200

    Commit Inicial 4

--- js/ejer17.js
+++ js/ejer17.js
@@ -4,6 +4,16 @@
 //(no es divisible por ninguno otro número que no sea él mismo o la unidad)
 
 let numero = prompt('Escribe un número: ')
-let sw=0
+let cont=0
 
-//
+for(let i=1; i<=numero; i++){
+   if(numero%i==0){
+    cont=cont+1     //Contar las veces que es divisible
+   }
+}
+if(cont==2){
+    console.log("El número es primo")   //Si son solo dos significa que el número es divisible por él  mismo y por 1
+}
+else{
+    console.log("El número NO es primo")
+}
